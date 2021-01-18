6e908f66b0f87ee7391dbe144767f7994550897a
Daniel Sasse <dsasse07@gmail.com>
2021-01-18 15:44:28 -0500

    Models & Routes

---
+++ db/migrate/20210118203156_create_addresses.rb
@@ -0,0 +1,17 @@
+class CreateAddresses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :addresses do |t|
+      t.string :street_1
+      t.string :street_2  
+      t.string :city
+      t.string :state
+      t.string :zip_code
+      t.integer :user_id
+      t.string :address_type
+      
+
+
+      t.timestamps
+    end
+  end
+end
