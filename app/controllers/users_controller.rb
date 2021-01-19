6ee8805b62ec0cc3a97b0c623ed0d8787ce2f3cb
Daniel Sasse <dsasse07@gmail.com>
2021-01-19 15:54:28 -0500

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,39 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+    # has_many relationship adds the collection.build method
+    @user.addresses.build(address_type: "Home")
+    @user.addresses.build(address_type: "Work")
+    @user.build_team # build_ method comes from the belongs_to association
+  end
+
+  def create
+    @user = User.create(user_params)
+    raise params.inspect
+    if @user.valid?
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, 
+                                addresses_attributes: [
+                                  :street_1,
+                                  :street_2,
+                                  :city,
+                                  :state,
+                                  :zip_code,
+                                  :address_type
+                                  ],
+                                team_attributes: [
+                                  :name,
+                                  :hometown
+                                ])
+  end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,35 @@
 class User < ApplicationRecord
   has_many :addresses
   belongs_to :team
+
+# We cannot call this custom writer addresses= because that would
+# Overwrite an AR method. The original AR method assumes you would
+# be taking in an array of address objects, not hash data.
+
+# We call this new method address_attributes instead
+  
+  # def addresses=(array_of_addresses)
+  # end
+
+  def addresses_attributes=(addresses_attributes)
+    addresses_attributes.each do |i, attribute_hash|
+      byebug
+      self.addresses.build(attribute_hash) unless attribute_hash[:street_1].blank?
+    end
+  end
+  # "addresses_attributes"=>
+  # {"0"=>{"street_1"=>"137 21st st", "street_2"=>"", "city"=>"NEW", "state"=>"ny", "zip_code"=>"98932", "address_type"=>"Home"},
+  #  "1"=>{"street_1"=>"", "street_2"=>"", "city"=>"", "state"=>"", "zip_code"=>"", "address_type"=>"Work"}},
+
+
+
+
+    # Finds team by name, if it does not find one, it will create one using the
+    # Attributes in the code block
+  def team_attributes=(team_attributes)
+    self.team = Team.find_or_create_by(name: team_attributes[:name]) do |team|
+      team.hometown = team_attributes[:hometown]
+    end
+  end
+
 end
